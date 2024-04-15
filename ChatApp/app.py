0531d7773e3f8cf02bfe870c9e43d2d1e9004fd0
6ixFeetWinter <104249266+6ixFeetWinter@users.noreply.github.com>
2024-04-15 22:16:01 +0900

    サインアップAPI

---
+++ ChatApp/app.py
@@ -0,0 +1,55 @@
+import datetime
+from flask import Flask, redirect, render_template, request, session, flash
+from datetime import timedelta
+import hashlib
+import uuid
+import re
+
+from util.DB import DB
+from config import config
+from models import models
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+
+@app.route('/signup')
+def signup():
+    return render_template('registration/signup.html')
+
+
+@app.route('/signup', methods=['POST'])
+def user_signup():
+  id = uuid.uuid4()
+  name = request.form.get('name')
+  email = request.form.get('email')
+  password1 = request.form.get('password1')
+  password2 = request.form.get('password2')
+  lng = request.form.get('language')
+  learning_lng = request.form.get('learning_language')
+  country = request.form.get('country')
+  city = request.form.get('city')
+
+  datetime = datetime.now()
+  last_operation_at = datetime.strftime('%Y-%m-%d %H:%M:%S')
+
+  pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+  if(name == "" | email == "" | password1 == "" | password2 == "" | lng == "" | learning_lng == ""):
+    flash("必須項目をすべて入力してください")
+  elif(password1 != password2):
+    flash("同じパスワードを入力してください")
+  elif(re.match(pattern,email)):
+    flash("正しいパスワードを入力してください")
+  else:
+    password = hashlib.sha256(password1.encode('utf-8')).hexdigest()
+    # TODO:既に登録されているユーザーか判定
+    # user = models.getUser(email)
+
+  #if user != None:
+  #  flash('既に登録済みのユーザーです')
+    models.create_user(id,name,email,password,lng,learning_lng,country,city,last_operation_at)
+    UserId = str(id)
+    session['uid'] = UserId
+    return redirect('/')
+  return redirect('/signup')
