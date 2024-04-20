4a7dd14967fa4520cf13852ef6245b427e94a190
yuki-fzy <7vemu052@gmail.com>
2024-04-20 23:44:18 +0900

    feature/login-api

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -4,7 +4,6 @@ import hashlib
 import uuid
 import re
 
-from util.DB import DB
 from config import config
 from models import models
 
@@ -42,15 +41,52 @@ def user_signup():
     flash("正しいパスワードを入力してください")
   else:
     password = hashlib.sha256(password1.encode('utf-8')).hexdigest()
-    # TODO:既に登録されているユーザーか判定
-    # user = models.getUser(email)
+    user = models.getUser(email)
 
-  #if user != None:
-  #  flash('既に使用されているアドレスです')
-    models.create_user(id,name,email,password,lng,learning_lng,country,city,last_operation_at)
-    UserId = str(id)
-    session['uid'] = UserId
-    return redirect('/')
+  if user != None:
+    flash('既に使用されているアドレスです')
+  else:
+     models.create_user(id,name,email,password,lng,learning_lng,country,city,last_operation_at)
+     UserId = str(id)
+     session['id'] = UserId
+     return redirect('/')
   return redirect('/signup')
+
+
+@app.route('/login')
+def login():
+    return render_template('registration/login.html')
+
+
+@app.route('/login', methods=['POST'])
+def userLogin():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+    if(email == "" & password == ""):
+       flash('メールアドレスとパスワードを入力してください')
+    elif(email == "" ):
+       flash('メールアドレスを入力してください')
+    elif(password == ""):
+       flash('パスワードを入力してください')
+    elif(re.match(pattern,email)):
+       flash('正しいメールアドレスを入力してください')
+    else:
+       user = models.getUser(email)
+       if user is None:
+          flash('存在しないユーザーです')
+       else:
+          password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+          if(password != user["password"]):
+             flash('パスワードが間違っています')
+          else:
+             session["id"] = user["id"]
+             datetime = datetime.now()
+             last_operation_at = datetime.strftime('%Y-%m-%d %H:%M:%S')
+             models.updateLastOperationAt(user["id"],last_operation_at)
+             return redirect('/')
+    return redirect('/login')
 
 if __name__ == '__main__': app.run(host="0.0.0.0", debug=False)
--- ChatApp/models.py
+++ ChatApp/models.py
@@ -10,7 +10,36 @@ class models:
       cursor.execute(sql, (id,name,email,password,lng,learning_lng,country,city,last_operation_at))
       connect.commit()
     except Exception as e:
-      print('エラー' + e)
+      print('エラー:' + e)
       abort(5000)
+    finally:
+      cursor.close()
+
+  def getUser(email):
+    try:
+      connect = DB.getConnection()
+      cursor = connect.cursor()
+      sql = "SELECT * FROM user WHERE email=%s;"
+      cursor.execute(sql, (email))
+      user = cursor.fetchone()
+      return user
+    except Exception as e:
+      print('エラー:' + e)
+      abort(500)
+    finally:
+      cursor.close()
+
+  # 最後に操作した日時を更新
+  def updateLastOperationAt(id,last_operation_at):
+    try:
+      connect = DB.getConnection()
+      cursor = connect.cursor()
+      sql = "UPDATE users SET last_operation_at=%s WHERE id=%s;"
+      cursor.execute(sql, (last_operation_at,id))
+      user = cursor.fetchone()
+      return user
+    except Exception as e:
+      print('エラー:' + e)
+      abort(500)
     finally:
       cursor.close()
