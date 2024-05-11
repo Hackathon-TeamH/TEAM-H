d3e454e866ab8fd1e50f85bf8e490f3218fee8d1
yuki-fzy <7vemu052@gmail.com>
2024-05-12 02:27:57 +0900

    ユーザーのinactive判定とactiveユーザーの取得

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -16,7 +16,9 @@ CREATE TABLE users(
   learning_language varchar(4) NOT NULL,
   country varchar(128),
   city varchar(128),
-  last_operation_at DATETIME NOT NULL
+  created_at DATETIME NOT NULL,
+  last_operation_at DATETIME NOT NULL,
+  is_active BOOLEAN NOT NULL
 );
 
 
@@ -41,12 +43,13 @@ CREATE TABLE memberships(
   channel_id varchar(255) REFERENCES channels(id)
 );
 
--- ハッシュ化後のパスワードを登録する必要がありそう（未対応）
+-- mysqlでは0がfalse0以外がtrueとして登録されるため1をインサート
 -- パスワードはpasswordをハッシュ化したもの
-INSERT INTO users(id, user_name, password, email, language, learning_language, country, city, last_operation_at) VALUES
-    ("35d485b3-f3e0-4b34-84bd-3460487c711e", "TEST_USER1", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "test1@email.com", "ja", "en", "日本", "東京", '2024-04-14 22:00:00');
-INSERT INTO users(id, user_name, password, email, language, learning_language, country, city, last_operation_at) VALUES
-    ("ab0bf204-3df1-4a52-b14a-89f18e8a8188", "TEST_USER2", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "test2@email.com", "en", "ja", "US", "LosAngeles", '2024-04-14 22:00:00');
+INSERT INTO users(id, user_name, password, email, language, learning_language, country, city, created_at,last_operation_at, is_active) VALUES
+    ("35d485b3-f3e0-4b34-84bd-3460487c711e", "TEST_USER1", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "test1@email.com", "ja", "en", "日本", "東京", '2024-04-14 22:00:00', '2024-04-14 22:00:00', 1);
+INSERT INTO users(id, user_name, password, email, language, learning_language, country, city, created_at,last_operation_at, is_active) VALUES
+    ("ab0bf204-3df1-4a52-b14a-89f18e8a8188", "TEST_USER2", "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8", "test2@email.com", "en", "ja", "US", "LosAngeles", '2024-04-14 22:00:00', '2024-04-14 22:00:00', 1);
+
 
 INSERT INTO messages (id, message, translated_message, created_at, user_id, channel_id) VALUES
     ("1", "Hello", "こんにちは", NOW(), "35d485b3-f3e0-4b34-84bd-3460487c711e", "1"),
